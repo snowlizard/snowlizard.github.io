10c1490bbe978d8ebebbf1d7a5c2574b5c49efd3
snowlizard <illegal_burrito@live.com>
2021-07-01 10:24:40 -0700

    about page done for now

--- src/components/about.js
+++ src/components/about.js
@@ -1,16 +1,32 @@
 import React from 'react';
+//import MnJ from '/public/images/about.jpeg';
 
 export const About = () => {
     return (
         <div>
             <div id="about">
-                <div id="imgDiv">
-                    <img src="../../public/images/about.jpg" id="JnM"></img>
+                <div className="cell" id="imgDiv">
+                    <img src={window.location.origin + '/images/about.jpeg'} id="JnM"></img>
+                </div>
+                <div className="cell" id="aboutText">
+                    <div id="aText">
+                        <div id="aboutHeadings">
+                            <h2>Julian Gonzalez</h2>
+                            <h3>Aspiring Developer</h3>
+                        </div>
+                        <p>
+                            Ugly boi here enjoys programming as a hobby and is also currently making a couple indie
+                            titles with his friends. 
+                            Education: Associates in Retardation
+                        </p>
+
+                    </div>
                 </div>
             </div>
             <div id="contact">
-
+                <h4>If you wish to contact me you can send me and email@</h4>
+                <h5>retardDev@gmail.com</h5>
             </div>
         </div>
     );
-}
+};
