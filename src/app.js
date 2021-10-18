aeb67de95ca4a1948e0c7bfe2ca8d43f08f0d0f9
snowlizard <illegal_burrito@live.com>
2021-10-17 17:56:31 -0700

    simple html portolio update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {MainRouter} from './router/mainRouter';
-import 'normalize.css';
-import './styles/styles.scss';
-
-ReactDOM.render(<MainRouter/>, document.getElementById('app'));
+$(window).on('resize', () => {
+    let win = $(this);
+    if( win.width() <= 800 ){
+      $('#git-icon').removeClass('fa-5x');
+      $('#git-icon').addClass('fa-2x')
+    }
+    
+    if( win.width() > 800 ){
+      $('#git-icon').removeClass('fa-2x')
+      $('#git-icon').addClass('fa-5x');
+    }
+    
+  })
